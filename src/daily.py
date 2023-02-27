1680d235337afe5ce8eceeae9d6bc6cc6d46e384
LinardsS <linards.solovjevs@gmail.com>
2023-02-27 10:25:44 +0200

    Daily file set up

---
+++ src/daily.py
@@ -0,0 +1,17 @@
+from downloadCsv import downloadPlayerStats
+#from uploadResults import uploadResultsAndStats
+from utils import getTodaysDate, getTomorrowsDate
+#from predictions import predictSlate, scorePredictions
+import sys
+from os import path
+basepath = path.dirname(__file__)
+todays_date = getTodaysDate()
+todays_date = todays_date + '.txt'
+filepath = path.abspath(path.join(basepath, "..", "data/daily logs", todays_date))
+sys.stdout = open(filepath, 'w')
+
+downloadPlayerStats(today = True, date = None, file_date = None) # downloads team stats file
+#uploadResultsAndStats()
+
+
+sys.stdout.close()
